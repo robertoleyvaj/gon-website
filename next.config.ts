8b0d34c7aa7779c875f5ef829df5f467d6370317
Roberto Leyva <robertoleyvaj@gmail.com>
2026-05-12 23:40:00 -0700

    fix webhook 307 redirect

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
   async headers() {
     return [
       {
