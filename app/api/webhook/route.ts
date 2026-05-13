ddfab7405501fc4a19628645c577dbee120df9cc
Roberto Leyva <robertoleyvaj@gmail.com>
2026-05-12 23:36:43 -0700

    fix webhook 307 redirect

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,7 +2,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 import { createClient } from '@supabase/supabase-js';
-
+export const dynamic = 'force-dynamic';
+export const runtime = 'nodejs';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 const supabase = createClient(
