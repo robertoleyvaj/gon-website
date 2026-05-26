c3d8bc26eb2b6705ea1fc00df9c0f23900d61c86
Roberto Leyva <robertoleyvaj@gmail.com>
2026-05-26 16:33:02 -0700

    Added Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,7 @@ export default function RootLayout({
         className="min-h-full flex flex-col"
         style={{ fontFamily: "var(--font-sans), sans-serif" }}
       >
+        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-QSNNCVBD50"
           strategy="afterInteractive"
@@ -103,6 +104,39 @@ export default function RootLayout({
             gtag('config', 'G-QSNNCVBD50');
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1001616152414051');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1001616152414051&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
 
         <LangProvider>
           <CartProvider>
