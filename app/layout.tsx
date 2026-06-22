503751eaa0c9c923599142c4cbd67d634d51ba39
Roberto Leyva <robertoleyvaj@gmail.com>
2026-06-22 13:14:48 -0700

    feat: add Meta Pixel 1036483012375096

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import { Fraunces, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { LangProvider } from "./components/LanguageContext";
 import { CartProvider } from "./context/CartContext";
@@ -97,6 +98,21 @@ export default function RootLayout({
         WebkitFontSmoothing: "antialiased",
         MozOsxFontSmoothing: "grayscale",
       }}>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1036483012375096');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <StructuredData />
         <LangProvider>
           <CartProvider>
